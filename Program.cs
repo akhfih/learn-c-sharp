44446a4964c052b2d4182cecbb2d8099d33ebc04
Akhmad Fikron Huda <akhmad.fikron.huda@gmail.com>
2024-03-10 20:18:39 +0700

    sorting

--- Program.cs
+++ Program.cs
@@ -83,6 +83,13 @@ while(counter2 < 5); */
 
 //Array
 //array lengt is fix
-var names = new String[] { "Scott", "Ana", "Felipe"};
-names = [..names,"Damina"];
+// var names = new String[] { "Scott", "Ana", "Felipe"};
+// names = [..names,"Damina"];
 
+//Sorting
+
+var numbers = new List<int> {45,56,99,48,67,78};
+
+Console.WriteLine($"I found 99 at index {numbers.IndexOf(99)}");
+numbers.Sort();
+Console.WriteLine($"I found 99 at index {numbers.IndexOf(99)}");
--- obj/Debug/net8.0/helloworld.AssemblyInfo.cs
+++ obj/Debug/net8.0/helloworld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("helloworld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+996de30c9ff1bb23914100802043f0d675587f2e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a933889c5363602368ecb1cf35a8cbcfe0b4679")]
 [assembly: System.Reflection.AssemblyProductAttribute("helloworld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("helloworld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
